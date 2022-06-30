55be417fb4dc7f8cd808898c6156f740c4880c45
epiqu1n <epiqu1n@gmail.com>
2022-06-29 20:02:04 -0400

    Parrot colors; set up simple script messaging
    Co-authored-by: Alina Gasperino <Alina207@users.noreply.github.com>
    Co-authored-by: alrokosz <alrokosz@users.noreply.github.com>
    Co-authored-by: erinzz <erinzz@users.noreply.github.com>

--- src/app/background.ts
+++ src/app/background.ts
@@ -1,6 +1,32 @@
-console.log('Running background script');
+// This script does not communicate with the DOM
+console.log('Running background script (see chrome extensions page)');
 
+// Globals
+let activeTabId: number;
+let recordedTabId: number;
+
+// Listen for messages from popup or content script
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-  console.log('Background got a message!');
-  sendResponse({});
+  console.log('Background got a message!', message);
+
+  switch (message.type) {
+    case 'begin-recording':
+      beginRecording();
+      break;
+  }
+  // sendResponse({});
+});
+
+/**
+ * Message the content script and instruct it to add event listeners and observer
+ */
+function beginRecording() {
+  recordedTabId = activeTabId;
+  chrome.tabs.sendMessage(recordedTabId, { type: 'message', payload: 'hello' });
+}
+
+/// Tab event listeners
+// On change tabs: Set active tab id to current tab id
+chrome.tabs.onActivated.addListener((activeInfo) => {
+  activeTabId = activeInfo.tabId;
 });
--- src/app/content.ts
+++ src/app/content.ts
@@ -1,3 +1,8 @@
+// This script has access to the DOM
+console.log('Running content script (see chrome devtools)');
+
+// Came with the template, idk what it does
+// Runs on page load
 chrome.runtime.sendMessage({}, (response) => {
   const checkReady = setInterval(() => {
     if (document.readyState === 'complete') {
@@ -5,4 +10,14 @@ chrome.runtime.sendMessage({}, (response) => {
       console.log('We\'re in the injected content script!');
     }
   });
+});
+
+// Listen for messages from background script
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+  switch (message.type) {
+    case 'message':
+      console.log('Content got a message! :', message.payload);
+      break;
+  }
+  // sendResponse({});
 });
