93f2d5630314cf00a8e2afdaaa1232bcf2ddcc2b
epiqu1n <epiqu1n@gmail.com>
2022-07-05 17:02:46 -0400

    Moved element states into background script

--- src/app/modules/eventListeners.ts
+++ src/app/modules/eventListeners.ts
@@ -7,22 +7,6 @@ export function stopEventListeners() {
 export function startEventListeners(){
   document.addEventListener('click', clickListener);
 }
-
-// const el = document.querySelector('article');
-
-// Define the types in our elementState map obj
-interface ElementState {
-  class?: string,
-  textContent?: string,
-  value?: string
-}
-
-// Initialize a Map obj
-const elementStates = new Map<HTMLElement, ElementState>();
-
-// Add key/val pair to the Map obj where the key is the el and the value is the element's state
-// elementStates.set(el, getCurrState(el));
-
 /*
 event: MouseEvent
 
@@ -43,36 +27,14 @@ function clickListener(this: Document, event: MouseEvent) {
   const selector = getSelector(target);
   console.log('Element clicked:', selector);
 
-  // Use array destructuring to check every el/state pair (treated like subarrays) in the map and find the differences between prev and curr state
-    // the stored state in the map at this point is now the previous state
-  for (const [el, prevState] of elementStates) {
-    const curState = getCurrState(el);
-    console.log(diffState(prevState, curState));
-    elementStates.set(el, curState);
-  }
+  chrome.runtime.sendMessage({
+    type: 'event-triggered',
+    payload: {
+      selector,
+      eventType: event.type
+    }
+  });
 
   // TODO: Will need an array or DS to push all of our state changes into to create tests
   // TODO: Also watch for new nodes using MutationObserver
-}
-
-// Declare a function that takes in an HTMLElement and outputs an obj representing the element state
-function getCurrState (el: HTMLElement | HTMLInputElement): ElementState {
-  return {
-    class: el.classList.value,
-    textContent: el.innerText,
-    value: 'value' in el ? el.value : undefined
-  };
-}
-
-// TODO: refactor to allow for mutilpe elements in a single state array
-function diffState(prev: ElementState, curr: ElementState): ElementState | null {
-  let differences: ElementState = null;
-  for (const _key in prev) {
-    const key = _key as keyof ElementState;
-    if (prev[key] !== curr[key]) {
-      if (!differences) differences = {};
-      differences[key] = curr[key];
-    }
-  }
-  return differences;
 }
