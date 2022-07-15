fedaa68d8213f34c80a684cf9318b977c1b49a76
Al Rokosz <Brokosz@Als-MacBook-Pro.local>
2022-07-15 13:11:05 -0700

    added wrong tab page

--- src/ui/App.tsx
+++ src/ui/App.tsx
@@ -6,16 +6,19 @@ import { Routes, Route, Navigate } from 'react-router-dom';
 import Recorder from './recorder/Recorder';
 import Tracking from './tracking/Tracking';
 import UserTests from './userTests/UserTests';
+import WrongTab from './components/WrongTab';
 
 
 export default function App() {
   const [isLoaded, setIsLoaded] = useState(false);
   const [recordingState, setRecordingState] = useState('');
   const [onCorrectTab, setOnCorrectTab] = useState(true);
+  const [recordingTab, setRecordingTab] = useState(null);
 
   useEffect(() => {
     chrome.runtime.sendMessage({type: 'popup-opened'}).then(res => {
       setRecordingState(res.recordingState);
+      setRecordingTab(res.recordedTabId);
       setIsLoaded(true);
       if (res.recordedTabId && (res.recordedTabId !== res.activeTabId)) setOnCorrectTab(false);
       if (res.recordingState === 'recording') {
@@ -25,7 +28,7 @@ export default function App() {
         setRecordingState('pre-recording');
       }
     });
-  }, []);
+  }, [onCorrectTab]);
 
 
   const application =
@@ -36,15 +39,20 @@ export default function App() {
       <Route path='/recorder' element={<Recorder
         recordingState={recordingState}
         setRecordingState={setRecordingState}
-      
-      />}></Route>
+      />}>
+      </Route>
       <Route path='/tracking' element={<Tracking />}></Route>
       <Route path='/userTests' element={<UserTests />}></Route>
       <Route path='*' element={<Navigate to='/recorder' />}></Route>
     </Routes>
   </>;
 
+  const wrongTab = <WrongTab
+    recordingTab={recordingTab}
+    setOnCorrectTab={setOnCorrectTab}
+  />;
+
   return (
-    isLoaded ? (onCorrectTab ? application : <h1>Wrong Tab</h1>) : <Loading/>
+    isLoaded ? (onCorrectTab ? application : wrongTab) : <Loading/>
   );
 }
