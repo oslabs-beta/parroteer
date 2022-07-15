ce01cd9e44a6f21cdfa5fae42c3fc081622e5702
epiqu1n <epiqu1n@gmail.com>
2022-07-15 16:54:36 -0400

    Added exportToFile documentation

--- src/app/modules/textExporter.ts
+++ src/app/modules/textExporter.ts
@@ -1,6 +1,10 @@
-export default function exportToFile(text: string, fileName: string, type = 'javascript') {
+/**
+ * Prompts the user to save a provided string of text under the given (suggested) file name
+ * @param type The mimetype of the file, e.g. `text/javascript`. Default 'javascript'
+ */
+export default function exportToFile(text: string, filename: string, type = 'javascript') {
   chrome.downloads.download({
-    filename: fileName,
+    filename: filename,
     url: `data:text/${type};charset=utf-8,${encodeURIComponent(text)}`,
     saveAs: true
   });
