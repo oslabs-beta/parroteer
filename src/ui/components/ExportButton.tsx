10abb86aed125c17bea3f820cc60a993953200ee
epiqu1n <epiqu1n@gmail.com>
2022-07-16 12:03:22 -0400

    Added functionality to export button

--- src/ui/components/ExportButton.tsx
+++ src/ui/components/ExportButton.tsx
@@ -1,10 +1,16 @@
-interface ExpectButtonProps {
-  onExport: () => void
+import exportToFile from '../../app/modules/textExporter';
+
+interface ExportButtonProps {
+  text: string
 }
 
-export default function ExpectButton({ onExport }: ExpectButtonProps) {
+export default function ExportButton({ text }: ExportButtonProps) {
+  const handleExport = () => {
+    exportToFile(text, 'test.js');
+  };
+
   return (
-    <button onClick={onExport}>
+    <button className='export hov' onClick={handleExport}>
       <img src='./icons/file_export.png'/>
     </button>
   );
