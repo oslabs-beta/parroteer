5d7430285fffd65100ad97f56d8e3fd1f4e8f7bd
epiqu1n <epiqu1n@gmail.com>
2022-07-19 20:43:48 -0400

    Committing changes to pull in dev; mostly done rewriting tests

--- __tests__/generateTests.test.ts
+++ __tests__/generateTests.test.ts
@@ -1,24 +1,77 @@
-import createTestsFromEvents from '../src/app/modules/generateTests';
+import createTestsFromEvents, { indent } from '../src/app/modules/generateTests';
 import endent from 'endent';
 import { UserInputEvent, PickedElementEvent, MutationEvent, StoredEvent } from '../src/types/Events';
 
+const debugScript = endent`
+  await page.evaluate(() => {
+    document.querySelector('#cover').addEventListener('click', () => {
+      document.querySelector('#cover > p').classList.toggle('test');
+      document.querySelector('#cover > h1').innerText = 'hi';
+    });
+  });
+`;
+const testURL = 'https://eloquentjavascript.net';
+
+function escapeRegExp(string: string) {
+  return string.replace(/[.*+?^${}()|[\]\\]/g, '\\$&'); // $& means the whole matched string
+}
+
+function stripWhitespace(string: string) {
+  return string.replace(/\s*/g, '');
+}
+
+function regexCode(string: string, tabs = 0, tabSize = 2) {
+  const indented = indent(string, tabs, tabSize)
+  return new RegExp(escapeRegExp(indented));
+}
+
+/* function escapeCode(string: string) {
+  return 
+} */
+
 describe('Basic test generation', () => {
-  it('should generate basic test setup with no inputs', () => {
-    const testResult = endent`
-      const puppeteer = require('puppeteer');
-
+  it('should generate basic test setup', () => {
+    const imports = `const puppeteer = require('puppeteer');`;
+    const describe = endent`
       describe('End-to-end test', () => {
-        it('passes this test', async () => {
-          const browser = await puppeteer.launch({ headless: false });
-          const page = await browser.newPage();
-          await page.goto('http://localhost:8080');
-
-
-          await browser.close();
-        });
+        /** @type {puppeteer.Browser} */ let browser;
+        /** @type {puppeteer.Page} */ let page;
+    `;
+    const before = endent`
+      beforeAll(async () => {
+        browser = await puppeteer.launch({ headless: false });
+        page = await browser.newPage();
+      });
+  `;
+    const testBlock = endent`
+      it('passes this test', async () => {
+        await page.goto('http://localhost:8080');
+        // Temporary variable to store elements when finding and making assertions
+        let element;
+    `;
+    const teardown = endent`
+      afterAll(() => {
+        await browser.close();
       });
     `;
-    expect(createTestsFromEvents([])).toEqual(testResult);
+
+    const generatedTests = createTestsFromEvents([], testURL, debugScript);
+    console.log(describe);
+    console.log(escapeRegExp(generatedTests));
+    console.log(!!generatedTests.match(describe));
+    
+    expect(generatedTests).toMatch(regexCode(imports));
+    expect(generatedTests).toMatch(regexCode(describe));
+    expect(generatedTests).toMatch(regexCode(before, 1));
+    expect(generatedTests).toMatch(indent(testBlock, 1));
+    expect(generatedTests).toMatch(indent(teardown, 1));
+  });
+
+  it('should display comment when tests events is empty', () => {
+    const noEventText = '// No events were recorded';
+    const generatedTests = createTestsFromEvents([], testURL, debugScript);
+    // console.log(generatedTests);
+    expect(generatedTests).toMatch(noEventText);
   });
 
   it('should generate a test for an event list with 1 picked-element event, 1 input event, and 1 mutation event', () => {
@@ -26,42 +79,38 @@ describe('Basic test generation', () => {
       type: 'input',
       eventType: 'click',
       parroteerId: '980458ad3-adsf34-df342-adsf898f',
-      selector: '#test > div'
+      selector: '#test > div',
+      displaySelector: ''
     };
     const mutationEvent: MutationEvent = {
       type: 'mutation',
       textContent: 'hi',
       class: 'class1 class2',
-      parroteerId: '980458ad3-adsf34-df342-adsf898f'
+      parroteerId: '980458ad3-adsf34-df342-adsf898f',
+      selector: '',
+      displaySelector: ''
     };
     const pickedElementEvent: PickedElementEvent = {
       type: 'picked-element',
-      initialSelector: '#test > div',
-      parroteerId: '980458ad3-adsf34-df342-adsf898f'
+      selector: '#test > div',
+      parroteerId: '980458ad3-adsf34-df342-adsf898f',
+      displaySelector: ''
     };
     
     const events: StoredEvent[] = [pickedElementEvent, inputEvent, mutationEvent];
     
-    const testResult = endent`
-      const puppeteer = require('puppeteer');
-
-      describe('End-to-end test', () => {
-        it('passes this test', async () => {
-          const browser = await puppeteer.launch({ headless: false });
-          const page = await browser.newPage();
-          await page.goto('http://localhost:8080');
-
-          await page.waitFor("#test > div").then(el => el.dataset.parroteerId = "980458ad3-adsf34-df342-adsf898f");
-          await page.click('[data-parroteer-id="980458ad3-adsf34-df342-adsf898f"]');
-          await page.waitFor('[data-parroteer-id="980458ad3-adsf34-df342-adsf898f"]').then(el => {
-            expect(el.textContent).toEqual('hi');
-            expect(el.classList.value).toEqual('class1 class2');
-          });
-
-          await browser.close();
-        });
+    const testScript = endent`
+      await page.waitForSelector('#test > div').then((el) => {
+        el.evaluate(el => el.dataset.parroteerId = '980458ad3-adsf34-df342-adsf898f');
       });
+      await page.waitForSelector('#test > div').then(el => el.click());
+      element = await page.$('[data-parroteer-id="980458ad3-adsf34-df342-adsf898f"]');
+      expect(getProp(element, 'textContent')).resolves.toEqual('hi');
+      expect(getProp(element, 'class')).resolves.toEqual('class1 class2');
     `;
-    expect(createTestsFromEvents(events)).toEqual(testResult);
+
+    const generatedTests = createTestsFromEvents(events, testURL, debugScript);
+    // console.log(generatedTests);
+    expect(generatedTests).toMatch(indent(testScript, 2));
   });
 });
